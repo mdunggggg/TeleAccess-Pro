a505482b13a30bfc684c6702dc03db7796ca259e
DUCNA1 <ducna1@hblab.vn>
2024-10-28 22:07:59 +0700

    ft-file: Update server file

--- src/main/java/com/example/teleaccesspro/server/file/ServerFileConnection.java
+++ src/main/java/com/example/teleaccesspro/server/file/ServerFileConnection.java
@@ -10,12 +10,12 @@ public class ServerFileConnection {
             System.out.println("Server File Transfer is running on port " + port);
             serverSocket = new ServerSocket(port);
 
-            while (true) {
-                System.out.println("Awaiting file connection from client");
-                Socket socket = serverSocket.accept();
-                System.out.println("Connection file established");
-                new ReceiveFileHandler(socket.getInputStream()).start();
-            }
+//            while (true) {
+//                System.out.println("Awaiting file connection from client");
+//                Socket socket = serverSocket.accept();
+//                System.out.println("Connection file established");
+//                new ReceiveFileHandler(socket.getInputStream()).start();
+//            }
         }
         catch (Exception e) {
             System.out.println("Error: " + e);
